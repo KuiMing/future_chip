b169b9723aa8afc21eb6e113ad6c63d9241638b4
KuiMing <benjamin0901@gmail.com>
2018-12-15 23:40:04 +0800

    output timeseries data

--- future_chip/dataset/get_future_history.py
+++ future_chip/dataset/get_future_history.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import wget
 import pandas as pd
 import zipfile
@@ -20,13 +21,17 @@ class GetFutureHistory():
         df = pd.read_csv(self.file, encoding='big5')
         df.商品代號 = [i.rstrip().lstrip() for i in df.商品代號]
         df['到期月份(週別)'] = [i.rstrip().lstrip() for i in df['到期月份(週別)']]
-        df = df[df['到期月份(週別)'] == df['到期月份(週別)'].unique()[0]]
+        df = df[df['到期月份(週別)'] == self._date.replace('/', '')[:6]]
         df = df[df['成交日期'] == int(self._date.replace('/', ''))]
         df = df[df.商品代號 == 'TX']
         df = df[df.成交時間 >=84500]
-        one_min_k = df.groupby(['成交時間'], as_index=False).max()
-        one_min_k['open'] = df.groupby(['成交時間'], as_index=False).first()['成交價格']
-        one_min_k['close'] = df.groupby(['成交時間'], as_index=False).last()['成交價格']
-        one_min_k['low'] = df.groupby(['成交時間'], as_index=False).min()['成交價格']
-        one_min_k = one_min_k.rename(columns={'成交價格':'high'})[['成交日期', '成交時間', 'open', 'close', 'high','low']]
-        return one_min_k
+        temp = df.groupby(['成交時間'], as_index=False).max()
+        temp['open'] = df.groupby(['成交時間'], as_index=False).first()['成交價格']
+        temp['close'] = df.groupby(['成交時間'], as_index=False).last()['成交價格']
+        temp['low'] = df.groupby(['成交時間'], as_index=False).min()['成交價格']
+        temp = temp.rename(columns={'成交價格':'high'})[['成交日期', '成交時間', 'open', 'close', 'high','low']]
+        date = []
+        for i in temp.成交時間:
+            date.append(datetime.strptime(str(temp.成交日期.iloc[0]) + " " + str(i), "%Y%m%d %H%M%S"))
+        output = pd.DataFrame({'open':temp.open.values, 'close':temp.close.values, 'high':temp.high.values, 'low':temp.low.values}, index=date)
+        return output
