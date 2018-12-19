c614985d27f7497b4523714d0d9ef9395fd40e95
KuiMing <benjamin0901@gmail.com>
2018-12-19 14:19:23 +0800

    get enviroment variable to control logger

--- future_chip/logger/logger.py
+++ future_chip/logger/logger.py
@@ -1,4 +1,17 @@
 import logging
+import os
+
+LOG_LEVEL_MAP = {
+    'DEBUG': logging.DEBUG,
+    'INFO': logging.INFO,
+    'WARN': logging.WARN,
+    'WARNING': logging.WARNING,
+    'ERROR': logging.ERROR,
+    'CRITICAL': logging.CRITICAL,
+    'FATAL': logging.FATAL
+}
+level = LOG_LEVEL_MAP.get(
+    os.getenv('LOG_LEVEL', 'ERROR').upper(), logging.ERROR)
 logger = logging.getLogger(__name__)
 logging.basicConfig(
-    format='%(levelname)s [ %(asctime)s ] %(message)s', level=logging.DEBUG)
+    format='%(levelname)s [ %(asctime)s ] %(message)s', level=level)
