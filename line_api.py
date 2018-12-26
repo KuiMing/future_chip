c4caef668c175e3322dd445287a3ceb771245e29
KuiMing <benjamin0901@gmail.com>
2018-12-26 11:53:54 +0800

    test flex message with json

--- line_api.py
+++ line_api.py
@@ -209,6 +209,52 @@ def handle_message(event):
             event.reply_token,
             message
         )
+    elif event.message.text == '1':
+        bubble = {
+                    "type": "bubble",
+                    "header": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                        {
+                            "type": "text",
+                            "text": "Header text"
+                        }
+                        ]
+                    },
+                    "hero": {
+                        "type": "image",
+                        "url": "https://example.com/flex/images/image.jpg"
+                    },
+                    "body": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                        {
+                            "type": "text",
+                            "text": "Body text"
+                        }
+                        ]
+                    },
+                    "footer": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                        {
+                            "type": "text",
+                            "text": "Footer text"
+                        }
+                        ]
+                    },
+                    "styles": {
+                        "comment": "See the example of a bubble style object"
+                    }
+                }
+        message = FlexSendMessage(alt_text="one", contents=bubble)
+        line_bot_api.reply_message(
+            event.reply_token,
+            message
+        )
     else:
         try:
             global table
